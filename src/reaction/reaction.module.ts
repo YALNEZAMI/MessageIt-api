3731f0229f0df307493d2f429692ce016c031350
YALNEZAMI <yaseralnezami@gmail.com>
2023-10-23 15:49:36 +0200

    desactive prettier

--- src/reaction/reaction.module.ts
+++ src/reaction/reaction.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { ReactionService } from './reaction.service';
 import { ReactionController } from './reaction.controller';
--- src/reaction/reaction.service.ts
+++ src/reaction/reaction.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { CreateReactionDto } from './dto/create-reaction.dto';
 import { UpdateReactionDto } from './dto/update-reaction.dto';
